e4d36446364c7c2a72e2d5e7d16156cb8f17dd0a
Milijana Tomic <mlgalic@gmail.com>
2020-05-19 22:47:55 -0500

    Providing function for naming current day

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 const APIkey = '8f3863754990ba6491b24790400e0681';
+const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 navigator.geolocation.getCurrentPosition(position => {
   homeLat = position.coords.latitude; 
@@ -11,22 +12,26 @@ navigator.geolocation.getCurrentPosition(position => {
         .then(response => response.json())
         .then(forecastData => {
           displayCurrentWeather(currentData);
-          //displayForecast(forecastData);
-          console.log(currentData);
-          console.log(forecastData.list);
+          //groupForecastData(forecastData.list);               
         });      
     });   
 });
 
-//https://www.w3schools.com/charsets/ref_utf_letterlike.asp (for Celsius degree)
+//source for HTML displaying Celsius degree sign: https://www.w3schools.com/charsets/ref_utf_letterlike.asp
 function displayCurrentWeather(query) {
   const currentElem = document.querySelector('.current-conditions');
   currentElem.insertAdjacentHTML('beforeend', 
-        `<img src="http://openweathermap.org/img/wn/${query.weather[0].icon}@2x.png">
-        <div class="current">
-          <div class="temp">${Math.round(query.main.temp)}&#8451;</div>
-          <div class="condition">${query.weather[0].description}</div>
-        </div>
-      </div>`
+    `<img src="http://openweathermap.org/img/wn/${query.weather[0].icon}@2x.png">
+    <div class="current">
+      <div class="temp">${Math.round(query.main.temp)}&#8451;</div>
+      <div class="condition">${query.weather[0].description}</div>
+    </div>`
   )
+}
+
+function nameCurrentDay() {  
+  const currentDayNum = new Date().getDay();
+  const currentDay = weekDays[currentDayNum - 1];
+
+  return currentDay;
 }
